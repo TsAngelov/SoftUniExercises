19b41ef5e5815a327bc158d9076cce226f9a07d0
TsAngelov <ts.angelov06@gmail.com>
2023-10-03 19:43:59 +0300

    .

---
+++ CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/06. Equal Sums/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Equal_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
