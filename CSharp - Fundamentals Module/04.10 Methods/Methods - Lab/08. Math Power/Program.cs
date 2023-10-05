8f0d9349214c92f1a0902aa90c932de57870b00c
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 00:39:06 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/04.10 Methods/Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bas = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(MathPow(bas,power));
+        }
+        static double MathPow (double bas, double power) 
+        {
+            return Math.Pow(bas, power);
+        }
+    }
+}
