b121e636efd260f13ed86a4e9270565d1fa35959
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 01:44:07 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/04.10 Methods/Methods - Lab/10. Multiply Even by Odds/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _10._Multiply_Even_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int result = 0;
+            result = GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+            return result;
+
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                if (number%10 % 2 == 0)
+                {
+                    sum += number % 10;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                if (number % 10 % 2 != 0)
+                {
+                    sum += number % 10;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
