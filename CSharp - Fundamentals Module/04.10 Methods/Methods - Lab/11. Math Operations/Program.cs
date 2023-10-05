0430bbcc256464056e44fa556a82e13c53db32f9
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 01:48:45 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/04.10 Methods/Methods - Lab/11. Math Operations/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _11._Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            char op = char.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(Calculation(num1,op,num2));
+        }
+        static double Calculation(double num1, char op, double num2)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    return num1 / num2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
