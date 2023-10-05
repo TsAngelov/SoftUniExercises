e66bf5705be47aca4046d36d061e9c6f9e0e5f89
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 00:32:54 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/04.10 Methods/Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
