2f4f5ca060de8a317ddfd945a5d8c795c781b40e
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 19:22:20 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/06.10 Methods - Exercise/Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            GetCharsBetween(char1, char2);
+        }
+
+        static void GetCharsBetween(char c1, char c2)
+        {
+            if (c1 > c2)
+            {
+                char temp = ' ';
+                temp = c1;
+                c1 = c2;
+                c2 = temp;
+            }
+            for (int i = (int)c1+1; i < (int)c2; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
