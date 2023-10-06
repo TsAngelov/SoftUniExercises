ec14a0a580c3188caca17a8f622eca4738bc18c9
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 19:14:19 +0300

    NEW SOLUTION

---
+++ CSharp - Fundamentals Module/06.10 Methods - Exercise/Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            GetVowelNum(input);
+        }
+        static void GetVowelNum (string input) 
+        {
+            int vowelNum = 0;
+            for (int i = 0; i < input.Length; i++) 
+            {
+                if ("AEIOUaeiou".Contains(input[i]))
+                    vowelNum++;
+            }
+            Console.WriteLine(vowelNum);
+        }
+    }
+}
