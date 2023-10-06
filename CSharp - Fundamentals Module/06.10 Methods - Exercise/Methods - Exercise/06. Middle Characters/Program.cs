8996f8c238ce92d48cbd984a087b41f93a01cd73
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 20:40:17 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/06.10 Methods - Exercise/Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
