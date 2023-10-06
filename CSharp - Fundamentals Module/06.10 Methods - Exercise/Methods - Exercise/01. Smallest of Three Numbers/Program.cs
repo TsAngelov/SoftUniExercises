7ffec799620cf360821e61e1cc1c89f9af2ef65b
TsAngelov <ts.angelov06@gmail.com>
2023-10-06 18:49:56 +0300

    New Exercise

---
+++ CSharp - Fundamentals Module/06.10 Methods - Exercise/Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            GetSmallestNum(num1, num2, num3);
+        }
+        static void GetSmallestNum (int num1, int num2, int num3)
+        {
+            int smallest = 0;
+            if (Math.Max(num1,num2) == num1)
+                smallest = num2;
+            else
+                smallest = num1;
+            if (smallest > num3)
+                smallest = num3;
+            Console.WriteLine(smallest);
+        }
+    }
+}
