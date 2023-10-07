d5fb1b116f3f30c71b04fd887f582df8cd105c52
TsAngelov <ts.angelov06@gmail.com>
2023-10-07 20:01:02 +0300

    New Solution

--- CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/07. Max Sequence of Equal Elements/Program.cs
+++ CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -5,13 +5,8 @@
         static void Main(string[] args)
         {
             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int start, end, previous = 0;
             for (int i = 0; i < numbers.Length; i++)
             {
-                if (numbers[i] = previous)
-                {
-
-                }
             }
         }
     }
--- CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/demo/Program.cs
+++ CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/demo/Program.cs
@@ -4,22 +4,13 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-
-                int[] tempArray = new int[numbers.Length];
-                int temp = numbers[0];
-                Array.Copy(numbers, 1, tempArray, 0, numbers.Length - 1);
-                tempArray[tempArray.Length - 1] = temp;
-                numbers = tempArray;
-
-            }
-            foreach (int number in numbers)
-            {
-                Console.Write(number + " ");
-            }
+            bool valid = false;
+            Asd(valid);
+            Console.WriteLine(Asd);
+        }
+        static bool Asd(bool test)
+        {
+            return test = true;
         }
     }
 }
