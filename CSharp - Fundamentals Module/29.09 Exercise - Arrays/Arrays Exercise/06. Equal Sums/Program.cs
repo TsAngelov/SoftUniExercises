3ce8bab26dc6d6012ed5462eee7bb3d9929c4d59
TsAngelov <ts.angelov06@gmail.com>
2023-10-07 17:04:47 +0300

    New Solution

--- CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/06. Equal Sums/Program.cs
+++ CSharp - Fundamentals Module/29.09 Exercise - Arrays/Arrays Exercise/06. Equal Sums/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
         }
     }
 }
