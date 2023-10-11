fa31ad1e5ebbb2c17959e452ac044468e9407201
TsAngelov <ts.angelov06@gmail.com>
2023-10-11 23:17:28 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            integers.Reverse();
+            integers.RemoveAll(n => n < 0);
+            if (integers.Count == 0)
+                Console.WriteLine("empty");
+            else
+                Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
