83cc895da1c23dd84f2b4a6522e50b11fd2fb842
TsAngelov <ts.angelov06@gmail.com>
2023-10-11 20:42:13 +0300

    New Exercise

---
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
