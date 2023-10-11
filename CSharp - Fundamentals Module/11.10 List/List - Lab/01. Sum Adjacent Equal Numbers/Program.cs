95eed8274f7b498b3c31fd9e186c3d336f331da3
TsAngelov <ts.angelov06@gmail.com>
2023-10-11 20:43:29 +0300

    New Exercise

--- CSharp - Fundamentals Module/11.10 List/List - Lab/01. Sum Adjacent Equal Numbers/Program.cs
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<double> numbers = new List<double>(Console.ReadLine().Split().Select(double.Parse).ToList());
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] += numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
