02027d6844700734f85703fa9ffa427bc842d5a8
TsAngelov <ts.angelov06@gmail.com>
2023-10-11 23:01:50 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/04. List of Products/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            list.Sort();
+            for (int i = 1; i <= list.Count; i++)
+            {
+                list[i - 1] = ($"{i}.{list[i - 1]}");
+            }
+            Console.WriteLine(string.Join("\n", list));
+        }
+    }
+}
