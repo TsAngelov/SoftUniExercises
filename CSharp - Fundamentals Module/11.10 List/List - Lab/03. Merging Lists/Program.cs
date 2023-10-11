5672ecb4030c5e9628020938bf2e2ebe8c6e65b3
TsAngelov <ts.angelov06@gmail.com>
2023-10-11 22:32:24 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/11.10 List/List - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list1 = new List<double>(Console.ReadLine().Split().Select(double.Parse).ToList());
+            List<double> list2 = new List<double>(Console.ReadLine().Split().Select(double.Parse).ToList());
+            List<double> result = new List<double>();
+            for (int i = 0; i < Math.Max(list1.Count, list2.Count); i++)
+            {
+                if (i < list1.Count)
+                {
+                    result.Add(list1[i]);
+                }
+                if (i < list2.Count)
+                {
+                    result.Add(list2[i]);
+                }  
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
