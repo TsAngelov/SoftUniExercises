54428f9b358d11d23e1c124381605ef0437c1090
TsAngelov <ts.angelov06@gmail.com>
2023-10-13 19:35:59 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/13.10 List - Exercise/List - Exercises/01. Train/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input.Contains("Add"))
+                {
+                    string[] command = input.Split().ToArray();
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + int.Parse(input) <= maxCapacity)
+                        {
+                            wagons[i] += int.Parse(input);
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
