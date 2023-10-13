06b71c475ed5f02000974c7004466a0a14452c14
TsAngelov <ts.angelov06@gmail.com>
2023-10-13 20:20:03 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/13.10 List - Exercise/List - Exercises/02. Change List/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] splitInput = input.Split().ToArray();
+                string command = splitInput[0];
+                int element = int.Parse(splitInput[1]);
+                switch (command)
+                {
+                    case "Delete": integers.RemoveAll(x => x == element); break;
+                    case "Insert": 
+                        int index = int.Parse(splitInput[2]);
+                        integers.Insert(index, element); break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
