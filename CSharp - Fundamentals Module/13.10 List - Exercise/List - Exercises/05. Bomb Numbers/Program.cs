ffcf96f3e08024498931ae9e433f21bcbe1519e7
TsAngelov <ts.angelov06@gmail.com>
2023-10-15 18:53:15 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/13.10 List - Exercise/List - Exercises/05. Bomb Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> factors = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int specNumber = factors[0];
+            int power = factors[1];
+            while (numbers.Contains(specNumber)) 
+            {
+                for (int i = 0; i < power; i++)
+                {
+                    int index = numbers.IndexOf(specNumber);
+                    if (index + 1 >= numbers.Count)
+                    {
+                    }
+                    else
+                        numbers.RemoveAt(index + 1);
+                    if (index - 1 >= numbers.Count)
+                    {
+                    }
+                    else
+                        numbers.RemoveAt(index - 1);
+                }
+                numbers.Remove(specNumber);
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
