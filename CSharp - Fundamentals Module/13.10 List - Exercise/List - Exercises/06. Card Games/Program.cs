16636324dc0c0041d0f26b6be5ecd70d01832ad9
TsAngelov <ts.angelov06@gmail.com>
2023-10-16 21:07:25 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/13.10 List - Exercise/List - Exercises/06. Card Games/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Card_Games
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
