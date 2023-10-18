172ac9af43875e8f6281becc04134d7371aa6ba1
TsAngelov <ts.angelov06@gmail.com>
2023-10-18 20:06:14 +0300

    Exam Prep

---
+++ CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/01.  Guinea Pig/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.__Guinea_Pig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
