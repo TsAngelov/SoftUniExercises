4a0c008c408bad1f0e7c3e039e894ad4a265439a
TsAngelov <ts.angelov06@gmail.com>
2023-10-18 21:15:51 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/02. The Lift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
