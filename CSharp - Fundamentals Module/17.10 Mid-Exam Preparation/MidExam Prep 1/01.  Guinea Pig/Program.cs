ccd2e863e87915d088e753bef5f60b73a7c9e7a1
TsAngelov <ts.angelov06@gmail.com>
2023-10-18 20:34:08 +0300

    New Solution

--- CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/01.  Guinea Pig/Program.cs
+++ CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/01.  Guinea Pig/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double kgFood = double.Parse(Console.ReadLine()) * 1000;
+            double kgHay = double.Parse(Console.ReadLine()) * 1000;
+            double kgCover = double.Parse(Console.ReadLine()) * 1000;
+            double kgGuinea = double.Parse(Console.ReadLine()) * 1000;
+            for (int i = 1; i <= 30; i++)
+            {
+                kgFood -= 300;
+                if (i%2 == 0)
+                    kgHay -= kgFood * 0.05;
+                if (i%3==0)
+                    kgCover -= kgGuinea / 3;
+                if (kgFood <= 0 || kgHay <= 0 || kgCover <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {kgFood/1000:f2}, Hay: {kgHay/1000:f2}, Cover: {kgCover / 1000:f2}.");
         }
     }
 }
