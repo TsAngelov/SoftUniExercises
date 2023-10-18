a44c6bdb68059080a590ab240661377fb1a3512f
TsAngelov <ts.angelov06@gmail.com>
2023-10-18 22:52:19 +0300

    New Solution

--- CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/02. The Lift/Program.cs
+++ CSharp - Fundamentals Module/17.10 Mid-Exam Preparation/MidExam Prep 1/02. The Lift/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int queue = int.Parse(Console.ReadLine());
+            int[] lift = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int max = 4;
+            for (int i = 0; i < lift.Length; i++)
+            {
+                if (lift[i] < 4)
+                {
+                    int emptySeats = 4 - lift[i];
+                    queue -= emptySeats;
+                    if (queue < 0)
+                    {
+                        lift[i] = 4 - Math.Abs(queue);
+                        Console.WriteLine("The lift has empty spots!");
+                        Console.WriteLine(string.Join(" ", lift));
+                        return;
+                    }
+                        lift[i] = 4;
+                }
+            }
+            if (queue != 0)
+            { 
+                Console.WriteLine($"There isn't enough space! {queue} people in a queue!");
+            }
+            Console.WriteLine(string.Join(" ", lift));
         }
     }
 }
