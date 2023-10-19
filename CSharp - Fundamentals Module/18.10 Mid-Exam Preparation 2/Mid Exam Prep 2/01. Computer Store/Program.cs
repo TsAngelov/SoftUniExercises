7642af04e9fd682a856fef4558c5f563d3c91e0c
TsAngelov <ts.angelov06@gmail.com>
2023-10-19 21:39:45 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/18.10 Mid-Exam Preparation 2/Mid Exam Prep 2/01. Computer Store/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _01._Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double total = 0, taxes = 0;
+            while (input != "special" && input != "regular")
+            {
+                double price = double.Parse(input);
+                if (price < 0)
+                    Console.WriteLine("Invalid price!");
+                else
+                {
+                    total += price;
+                    taxes += price * 0.2;
+                }
+                input = Console.ReadLine();
+            }
+            double totalPrice = total + taxes;
+            if (totalPrice > 0)
+            {
+                if (input == "regular")
+                {
+                    Console.WriteLine("Congratulations you've just bought a new computer!");
+                    Console.WriteLine($"Price without taxes: {total:f2}$");
+                    Console.WriteLine($"Taxes: {taxes:f2}$");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine($"Total price: {totalPrice:f2}$");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations you've just bought a new computer!");
+                    Console.WriteLine($"Price without taxes: {total:f2}$");
+                    Console.WriteLine($"Taxes: {taxes:f2}$");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine($"Total price: {totalPrice - totalPrice * 0.1:f2}$");
+                }
+            }
+            else
+                Console.WriteLine("Invalid order!");
+            
+        }
+    }
+}
