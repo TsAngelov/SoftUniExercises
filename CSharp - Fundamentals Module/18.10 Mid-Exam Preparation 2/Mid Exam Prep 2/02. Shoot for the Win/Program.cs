d1c59e5309803d93038d8395a8c5ca0f836a83a1
TsAngelov <ts.angelov06@gmail.com>
2023-10-21 05:38:13 +0300

    New Solution

--- CSharp - Fundamentals Module/18.10 Mid-Exam Preparation 2/Mid Exam Prep 2/02. Shoot for the Win/Program.cs
+++ CSharp - Fundamentals Module/18.10 Mid-Exam Preparation 2/Mid Exam Prep 2/02. Shoot for the Win/Program.cs
@@ -12,12 +12,33 @@
                 int index = int.Parse(input);
                 if (index >= 0 && index < targets.Count)
                 {
-
+                    if (targets[index] == -1)
+                    {
+                    }
+                    else
+                    {
+                        for (int i = 0; i < targets.Count; i++)
+                        {
+                            if (i != index && targets[i] != -1)
+                            {
+                                if (targets[i] > targets[index])
+                                    targets[i] -= targets[index];
+                                else if (targets[i] <= targets[index])
+                                {
+                                    targets[i] += targets[index];
+                                }
+                                    
+                            }
+                        }
+                        targets[index] = -1;
+                        targetsShot++;
+                    }
                 }
                 else
                 {
                 }
             }
+            Console.WriteLine($"Shot targets: {targetsShot} -> {string.Join(" ", targets)}");
         }
     }
 }
