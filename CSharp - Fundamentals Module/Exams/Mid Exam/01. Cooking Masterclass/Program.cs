f202229530da0408b055eb08dc753dcc2565d665
TsAngelov <ts.angelov06@gmail.com>
2023-10-22 12:23:09 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/Exams/Mid Exam/01. Cooking Masterclass/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Cooking_Masterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPrice = double.Parse(Console.ReadLine());
+            double eggPrice = double.Parse(Console.ReadLine());
+            double apronPrice = double.Parse(Console.ReadLine());
+
+            double flourCost = flourPrice * (students - (students / 5));
+            double eggCost = eggPrice * 10 * students;
+            double apronCost = apronPrice * (students + (Math.Ceiling(students * 0.2)));
+            double price = flourCost + eggCost + apronCost;
+
+            if (price <= budget)
+                Console.WriteLine($"Items purchased for {price:f2}.$");
+            else
+                Console.WriteLine($"{price - budget:f2}$ more needed.");
+        }
+    }
+}
