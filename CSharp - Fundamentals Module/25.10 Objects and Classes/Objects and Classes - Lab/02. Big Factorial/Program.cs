cc5a692a1689424dbe554cf79de58ec138ab349c
TsAngelov <ts.angelov06@gmail.com>
2023-10-25 20:37:39 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger f = 1;
+            for (int i = n; i >= 2; i--)
+            {
+                f *= i;
+            }
+            Console.WriteLine(f);
+        }
+    }
+}
---
+++ CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
