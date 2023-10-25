02520855ca2ee30ae9bca079273e954286d95902
TsAngelov <ts.angelov06@gmail.com>
2023-10-25 20:10:12 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Random random = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int randomIndex = random.Next(input.Length);
+
+                string currentWord = input[i];
+                string randomWord = input[randomIndex];
+
+                input[i] = randomWord;
+                input[randomIndex] = currentWord;
+            }
+            Console.WriteLine(string.Join("\n", input));
+        }
+    }
+}
--- CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/demo/Program.cs
+++ CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/demo/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Person();
+            person.FirstName = "Petar";
+            person.LastName = Console.ReadLine();
+            person.Walk();
+
+            Person person2 = new Person()
+            {
+                FirstName = "Ivan",
+                LastName = "Petrakiev"
+            };
+
+            Person person3 = person2;
+            person3.FirstName = "Petraki";
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+
+            string test = person.LastName;
+
+            TestMethod(person.LastName);
+        }
+        static void TestMethod(string name)
+        {
+            Console.WriteLine("hello " + name);
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }  // public *type* *proterty name* { get; set; }
+        public string LastName { get; set; }
+
+        public void Walk()
+        {
+            Console.WriteLine("I am walking");
         }
     }
 }
