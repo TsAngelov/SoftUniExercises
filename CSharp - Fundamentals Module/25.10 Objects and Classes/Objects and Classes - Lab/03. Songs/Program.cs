37f0a0c267e37e6e6dd9ed9cd686f42c4c329ce5
TsAngelov <ts.angelov06@gmail.com>
2023-10-25 20:48:37 +0300

    New Solution

--- CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/03. Songs/Program.cs
+++ CSharp - Fundamentals Module/25.10 Objects and Classes/Objects and Classes - Lab/03. Songs/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            List<Song> playlist = new List<Song>();
+            for (int i = 0; i < n; i++) 
+            {
+                string[] lineTokens = Console.ReadLine().Split('_');
+                string listType = lineTokens[0];
+                string songName = lineTokens[1];
+                string songTime = lineTokens[2];
+
+                Song song = new Song()
+                {
+                    Name = songName,
+                    TypeList = listType,
+                    Time = songTime
+                };
+                playlist.Add(song);
+            }
+            string filter = Console.ReadLine();
+            if (filter != "all")
+            {
+                for (int i = 0; i < playlist.Count; i++)
+                {
+                    Song currentSong = playlist[i];
+                    if (currentSong.TypeList != filter)
+                    {
+                        playlist.RemoveAt(i);
+                    }
+                }
+            }
+        } 
+
+        public class Song
+        {
+            public string TypeList { get; set; }
+            public string Name { get; set; }
+            public string Time { get; set; }
         }
+
     }
 }
