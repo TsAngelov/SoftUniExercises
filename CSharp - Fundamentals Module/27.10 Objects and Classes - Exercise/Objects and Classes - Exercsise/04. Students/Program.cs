2d16f648e651888684d0c29dd71a6502bcd7bc22
TsAngelov <ts.angelov06@gmail.com>
2023-10-27 20:39:20 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/27.10 Objects and Classes - Exercise/Objects and Classes - Exercsise/04. Students/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Student> list = new List <Student>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                Student student = new Student(input[0], input[1], double.Parse(input[2]));
+                list.Add(student);
+            }
+            list = list.OrderByDescending(student => student.Grade).ToList();
+            Console.WriteLine(string.Join("\n", list));
+        }
+    }
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
