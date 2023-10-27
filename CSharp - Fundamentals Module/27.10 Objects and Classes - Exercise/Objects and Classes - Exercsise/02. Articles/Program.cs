8ee962c845ba909198da05b0000c80e69f6f62a6
TsAngelov <ts.angelov06@gmail.com>
2023-10-27 19:52:32 +0300

    New Solution

---
+++ CSharp - Fundamentals Module/27.10 Objects and Classes - Exercise/Objects and Classes - Exercsise/02. Articles/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _02._Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            Article article = new Article(input[0], input[1], input[2]);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) 
+            {
+                string[] lineTokens = Console.ReadLine().Split(": ");
+                string command = lineTokens[0];
+                string text = lineTokens[1];
+                switch (command)
+                {
+                    case "Edit:":
+                        article.Edit(text);
+                        break;
+                    case "ChangeAuthor:":
+                        article.ChangeAuthor(text);
+                        break;
+                    case "Rename:":
+                        article.Rename(text);
+                        break;
+                }
+            }
+            Console.WriteLine(article);
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
