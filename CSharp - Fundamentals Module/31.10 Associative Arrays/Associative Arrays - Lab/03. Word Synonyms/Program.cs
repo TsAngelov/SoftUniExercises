84d4bf51a3bc95e280c594aa3ca8e37bfd73912e
TsAngelov <ts.angelov06@gmail.com>
2023-10-31 19:47:50 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/31.10 Associative Arrays/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
