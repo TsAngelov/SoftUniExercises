e59376d52f94b8413e857110d642c944be569562
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 20:14:55 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿ namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while((input = Console.ReadLine()) != "end") 
+            {
+                Console.WriteLine($"{input} = {new string(input.Reverse().ToArray())}");
+            }
+        }
+    }
+}
