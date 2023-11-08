0839eb971d09f69301d3dadb538d55243e4ae25f
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 20:53:18 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filters = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (string filter in filters)
+            {
+                string replacement = new string('*', filter.Length);
+                text = text.Replace(filter, replacement);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/5. Digits, Letters and Others/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5._Digits__Letters_and_Others
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
