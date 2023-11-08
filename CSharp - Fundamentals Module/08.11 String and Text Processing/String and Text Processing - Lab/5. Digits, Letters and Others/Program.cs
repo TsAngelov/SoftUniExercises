92c3796028d3ded6642032245295457daf392cb7
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 21:13:42 +0200

    New Solution

--- CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/5. Digits, Letters and Others/Program.cs
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/5. Digits, Letters and Others/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+            Console.WriteLine(input.Where(x => Char.IsDigit(x)).ToArray());
+            Console.WriteLine(input.Where(x => Char.IsLetter(x)).ToArray());
+            Console.WriteLine(input.Where(x => !Char.IsLetterOrDigit(x)).ToArray());
         }
     }
 }
