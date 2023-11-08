05ca3b64c093c45209fec5aa5a629fadc8ebb0be
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 12:05:23 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/03.11 Associative Arrays - Exercise/Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+            foreach (string word in words) 
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (countChars.ContainsKey(word[i]))
+                    {
+                        countChars[word[i]]++;
+                    }
+                    else
+                    {
+                        countChars.Add(word[i], 1);
+                    }
+                }
+            }
+            foreach (var item in countChars)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
