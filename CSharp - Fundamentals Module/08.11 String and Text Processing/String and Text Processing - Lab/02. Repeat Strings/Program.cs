e9b5ee3c91ceeb5e4fe0d2907d09f04dd8f2d1a1
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 20:22:35 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            StringBuilder result = new StringBuilder();
+            foreach (string word in words) 
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    result.Append(word);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
