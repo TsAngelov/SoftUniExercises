e8644fb3364f4a175ec670b8527d94759bf143a5
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 12:35:14 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/03.11 Associative Arrays - Exercise/Associative Arrays - Exercise/03. Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,double> orders = new Dictionary<string,double>();
+            string input;
+            while ((input = Console.ReadLine()) != "buy") 
+            {
+                string[] order = input.Split();
+                string product = order[0];
+                double price = double.Parse(order[1]);
+                int quantity = int.Parse(order[2]);
+                if (orders.ContainsKey(product))
+                {
+                    orders[product] += price * quantity;      
+                }
+                else
+                {
+                    orders.Add(product, price * quantity);    // price replaces with each  // quantity adds on
+                }
+            }
+            foreach (var item in orders)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+            }
+        }
+    }
+}
