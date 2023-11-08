22d9555ac272f616fd5b44d742c625e0b9f42275
TsAngelov <ts.angelov06@gmail.com>
2023-11-08 20:43:57 +0200

    New Solution

--- CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/03. SubString/Program.cs
+++ CSharp - Fundamentals Module/08.11 String and Text Processing/String and Text Processing - Lab/03. SubString/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            string substring = Console.ReadLine();
+            string word = Console.ReadLine();
+            while (word.Contains(substring))
+            {
+                int startindex = word.IndexOf(substring);
+                word = word.Remove(startindex, substring.Length);
+            }
+            Console.WriteLine(word);
         }
     }
 }
