f2a94a9a5ee1a0ead427f0ad3e6ed79bc95c84a8
TsAngelov <ts.angelov06@gmail.com>
2023-11-10 21:45:10 +0200

    New Solution

--- CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/05. Multiply BigNum/Program.cs
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/05. Multiply BigNum/Program.cs
@@ -4,9 +4,41 @@
     {
         static void Main(string[] args)
         {
-            double firstNum = double.Parse(Console.ReadLine());
-            int secondNum = int.Parse(Console.ReadLine());
-            Console.WriteLine((firstNum*secondNum).ToString("F0"));
+            static void Main(string[] args)
+            {
+                string bigNumber = Console.ReadLine();
+                string multiplyNumber = Console.ReadLine();
+
+                Console.WriteLine(MultiplyNumbers(bigNumber, multiplyNumber));
+            }
+
+            private static string MultiplyNumbers(string bigNumber, string multiplyNumber)
+            {
+                if (bigNumber == "0" || multiplyNumber == "0")
+                {
+                    return "0";
+                }
+                int carry = 0;
+                int multiplier = int.Parse(multiplyNumber);
+
+                char[] result = new char[bigNumber.Length + 1];
+
+                for (int i = bigNumber.Length - 1; i >= 0; i--)
+                {
+                    int digit = int.Parse(bigNumber[i].ToString());
+                    int currentSum = digit * multiplier + carry;
+
+                    result[i + 1] = (char)(currentSum % 10 + '0');
+                    carry = currentSum / 10;
+                }
+
+                if (carry > 0)
+                {
+                    result[0] = (char)(carry + '0');
+                }
+
+                return new string(result).TrimStart('\0');
+            }
         }
     }
 }
---
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/06. Replace Repeat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Replace_Repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
