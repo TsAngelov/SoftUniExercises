39ed6c42cf323f4dc375e0edd3747c197a6a3001
TsAngelov <ts.angelov06@gmail.com>
2023-11-10 21:52:15 +0200

    New Solution

--- CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/06. Replace Repeat/Program.cs
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/06. Replace Repeat/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace _06._Replace_Repeat
+﻿using System.Text;
+
+namespace _06._Replace_Repeat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            StringBuilder resutBuilder = new StringBuilder();
+            resutBuilder.Append(input[0]);
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i-1])
+                {
+                    resutBuilder.Append(input[i]);
+                }
+            }
+            Console.WriteLine(resutBuilder);
         }
     }
 }
