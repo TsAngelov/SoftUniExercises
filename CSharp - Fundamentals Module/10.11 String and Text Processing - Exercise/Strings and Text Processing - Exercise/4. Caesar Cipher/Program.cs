e8fa5aee6153740a4231301708bad361e00298a9
TsAngelov <ts.angelov06@gmail.com>
2023-11-10 21:12:20 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/4. Caesar Cipher/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _4._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string encryptedText = "";
+            foreach (char character in text)
+                encryptedText += (char)(character + 3);
+
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
