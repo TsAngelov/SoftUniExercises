bfe8f849dba965221c415d86a156fa2a25bec018
TsAngelov <ts.angelov06@gmail.com>
2023-11-10 21:09:40 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split('\\');
+            string[] file = path[path.Length - 1].Split('.');
+            Console.WriteLine($"File name: {file[0]}");
+            Console.WriteLine($"File extension: {file[1]}");
+        }
+    }
+}
