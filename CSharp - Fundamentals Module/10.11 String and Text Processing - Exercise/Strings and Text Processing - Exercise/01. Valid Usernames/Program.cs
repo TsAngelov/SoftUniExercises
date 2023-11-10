09aac2ed064deda41866205b8afebeb8f918a8c7
TsAngelov <ts.angelov06@gmail.com>
2023-11-10 20:56:38 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/10.11 String and Text Processing - Exercise/Strings and Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            foreach (string username in usernames) 
+            {
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    if (username.All(
+                        ch => char.IsLetterOrDigit(ch) ||
+                        ch == '-' ||
+                        ch == '_'))
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+            }
+        }
+    }
+}
