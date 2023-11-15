918c67c2ae69a587d64328df5c9351381695206a
TsAngelov <ts.angelov06@gmail.com>
2023-11-15 20:27:41 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/15.11 Regular Expressions - Lab/Regex - Exercise/01. Match Full Name/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
