1fb5ca4141c592d932e81d0fcce0869e5e000bb0
TsAngelov <ts.angelov06@gmail.com>
2023-11-15 21:32:58 +0200

    New Solution

--- CSharp - Fundamentals Module/15.11 Regular Expressions - Lab/Regex - Lab/01. Match Full Name/obj/Debug/net6.0/01. Match Full Name.AssemblyInfo.cs
+++ CSharp - Fundamentals Module/15.11 Regular Expressions - Lab/Regex - Lab/01. Match Full Name/obj/Debug/net6.0/01. Match Full Name.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Match Full Name")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5141ae1481bfdee379f8563264e5b4aa9e0d6c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Match Full Name")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Match Full Name")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSharp - Fundamentals Module/15.11 Regular Expressions - Lab/Regex - Lab/02. Match Phone Number/Program.cs
+++ CSharp - Fundamentals Module/15.11 Regular Expressions - Lab/Regex - Lab/02. Match Phone Number/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace _02._Match_Phone_Number
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string regex = @"\+359([ -])2(\1)\d{3}(\1)\d{4}\b";
+            string names = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(names, regex);
+
+            var matchedPhones = matches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhones));
         }
     }
 }
