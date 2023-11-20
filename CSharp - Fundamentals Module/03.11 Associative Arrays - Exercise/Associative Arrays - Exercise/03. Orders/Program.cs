e72bfeee1ed29783d7f66a96ed7986f9b731d970
TsAngelov <ts.angelov06@gmail.com>
2023-11-20 06:30:19 +0200

    New Solution

--- CSharp - Fundamentals Module/03.11 Associative Arrays - Exercise/Associative Arrays - Exercise/03. Orders/Program.cs
+++ CSharp - Fundamentals Module/03.11 Associative Arrays - Exercise/Associative Arrays - Exercise/03. Orders/Program.cs
@@ -4,27 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Dictionary<string,double> orders = new Dictionary<string,double>();
+            Dictionary<string, Product> orders = new Dictionary<string, Product>();
             string input;
-            while ((input = Console.ReadLine()) != "buy") 
+            while ((input = Console.ReadLine()) != "buy")
             {
-                string[] order = input.Split();
-                string product = order[0];
-                double price = double.Parse(order[1]);
-                int quantity = int.Parse(order[2]);
-                if (orders.ContainsKey(product))
+                string[] arguments = input.Split();
+                string product = arguments[0];
+                decimal price = decimal.Parse(arguments[1]);
+                int quantity = int.Parse(arguments[2]);
+
+                Product productInfo = new Product(product, price, quantity);
+                if (!orders.ContainsKey(product))
                 {
-                    orders[product] += price * quantity;      
+                    orders.Add(productInfo.Name, productInfo);
                 }
                 else
                 {
-                    orders.Add(product, price * quantity);    // price replaces with each  // quantity adds on
+                    orders[product].Update(productInfo.Price, productInfo.Quantity);
                 }
             }
             foreach (var item in orders)
             {
-                Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+                Console.WriteLine(item.Value);
             }
         }
     }
+    public class Product
+    {
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public void Update(decimal price, int quantity)
+        {
+            Price = price;
+            Quantity += quantity;
+        }
+        public decimal GetTotal()
+        {
+            return Price * Quantity;
+        }
+        public override string ToString()
+        {
+            return $"{Name} -> {GetTotal():f2}";
+        }
+    }
+}
 }
