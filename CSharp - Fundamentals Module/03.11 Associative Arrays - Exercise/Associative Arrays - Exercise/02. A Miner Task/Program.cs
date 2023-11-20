bb0cd334aa4fa3af70b0e3a48084f93c77cd3461
TsAngelov <ts.angelov06@gmail.com>
2023-11-20 06:31:12 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/03.11 Associative Arrays - Exercise/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ores = new Dictionary<string, int>();
+            string input;
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string ore = input;
+                int quantity = int.Parse(Console.ReadLine());
+                if (ores.ContainsKey(ore))
+                {
+                    ores[ore] += quantity;
+                }
+                else
+                {
+                    ores.Add(ore, quantity);
+                }
+            }
+            foreach (var item in ores)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
