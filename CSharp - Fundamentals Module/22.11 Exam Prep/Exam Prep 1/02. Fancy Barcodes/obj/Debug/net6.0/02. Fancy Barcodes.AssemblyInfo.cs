5c79275e04bf6ed63109249f1ab803e09ed70d69
TsAngelov <ts.angelov06@gmail.com>
2023-11-22 23:56:19 +0200

    New Solution

--- CSharp - Fundamentals Module/22.11 Exam Prep/Exam Prep 1/02. Fancy Barcodes/obj/Debug/net6.0/02. Fancy Barcodes.AssemblyInfo.cs
+++ CSharp - Fundamentals Module/22.11 Exam Prep/Exam Prep 1/02. Fancy Barcodes/obj/Debug/net6.0/02. Fancy Barcodes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02. Fancy Barcodes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7556a171331afc527f31bee7f52f0c7b2420500c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("02. Fancy Barcodes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02. Fancy Barcodes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Fundamentals Module/22.11 Exam Prep/Exam Prep 1/03. Pirates/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _03._Pirates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, City> cities = new Dictionary<string, City>();
+            string input;
+            while ((input = Console.ReadLine()) != "Sail")
+            {
+                string[] arg = input.Split("||");
+                string cityName = arg[0];
+                int population = int.Parse(arg[1]);
+                int gold = int.Parse(arg[2]);
+                if (cities.ContainsKey(cityName))
+                {
+                    cities[cityName].Population += population;
+                    cities[cityName].Gold += gold;
+                }
+                else
+                    cities.Add(cityName, new City(population, gold));
+            }
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] arg = input.Split("=>");
+                string command = arg[0];
+                string town = arg[1];
+                if (command == "Plunder")
+                {
+                    int people = int.Parse(arg[2]);
+                    int gold = int.Parse(arg[3]);
+                    cities[town].Population -= people;
+                    cities[town].Gold -= gold;
+                    if (cities[town].Population <= 0 || cities[town].Gold <= 0)
+                    {
+                        cities.Remove(town);
+                        Console.WriteLine($"{town} has been wiped off the map!");
+                    }
+                    else
+                        Console.WriteLine($"{town} plundered! {gold} gold stolen, {people} citizens killed.");
+                }
+                else
+                {
+                    int gold = int.Parse(arg[2]);
+                    if (gold < 0)
+                        Console.WriteLine("Gold added cannot be a negative number!");
+                    else
+                    {
+                        cities[town].Gold += gold;
+                        Console.WriteLine($"{gold} gold added to the city treasury. {town} now has {cities[town].Gold} gold.");
+                    }
+                }
+            }s
+            foreach (var city in City)
+            {
+
+            }
+
+        }
+    }
+    public class City
+    {
+        public City(int population, int gold)
+        {
+            Population = population;
+            Gold = gold;
+        }
+        public int Population { get; set; }
+        public int Gold { get; set; }
+    }
+}
