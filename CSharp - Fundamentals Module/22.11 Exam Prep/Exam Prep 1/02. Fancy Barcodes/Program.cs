b4d868409493de2f8e87da84d774377a3a3a6d97
TsAngelov <ts.angelov06@gmail.com>
2023-11-22 19:55:10 +0200

    New Solution

---
+++ CSharp - Fundamentals Module/22.11 Exam Prep/Exam Prep 1/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"(@#+)(?<barcode>[A-Z]{1}[A-Za-z-0-9]{4,}[A-Z]{1})(@#+)";
+            RegexOptions options = RegexOptions.Multiline;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                GroupCollection groupCollection = Regex.Match(input, pattern, options).Groups;
+                if (groupCollection.Count != 1)
+                {
+                    string barcode = groupCollection["barcode"].Value;
+                    string barcodeDigits = new string(barcode.Where(x => char.IsDigit(x)).ToArray());
+                    if (barcodeDigits.Length == 0)  
+                        Console.WriteLine("Product group: 00");
+                    else
+                        Console.WriteLine($"Product group: {barcodeDigits}");
+                }
+                else
+                    Console.WriteLine("Invalid barcode");
+            }
+        }
+    }
+}
