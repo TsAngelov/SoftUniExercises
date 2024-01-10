3b0e302178559497f3e17dd8e8e458f245e1eeda
TsAngelov <ts.angelov06@gmail.com>
2024-01-10 22:05:01 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/09.01 - Stacks and Queues/07. Hot Potato/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>(Console.ReadLine().Split());
+            int n = int.Parse(Console.ReadLine());
+            int throws = 1;
+            while (names.Count != 1)
+            {
+                string personWithPotato = names.Dequeue();
+                if (throws == n)
+                {
+                    Console.WriteLine($"Removed {personWithPotato}");
+                    throws = 0;
+                }
+                else
+                {
+                    names.Enqueue(personWithPotato);
+                }
+                throws++;
+            }
+            Console.WriteLine($"Last is {names.Dequeue()}");
+        }
+    }
+}
--- CSharp - Advanced/C# Advanced/09.01 - Stacks and Queues/demo/Program.cs
+++ CSharp - Advanced/C# Advanced/09.01 - Stacks and Queues/demo/Program.cs
@@ -4,23 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Stack<int> newStack = new Stack<int>();
+            //Stack<int> newStack = new Stack<int>();
 
-            newStack.Push(5);
-            newStack.Push(53);
-            newStack.Push(1);
-            newStack.Push(17);
+            //newStack.Push(5);
+            //newStack.Push(53);
+            //newStack.Push(1);
+            //newStack.Push(17);
 
-            //int topElement = newStack.Pop();
+            ////int topElement = newStack.Pop();
 
-            //Console.WriteLine(topElement);
+            ////Console.WriteLine(topElement);
 
-            //Console.WriteLine(newStack.Pop());
+            ////Console.WriteLine(newStack.Pop());
 
-            while (newStack.Count != 0)
+            //while (newStack.Count != 0)
+            //{
+            //    int topElement = newStack.Pop();
+            //    Console.WriteLine(topElement);
+            //}
+
+            Queue<string> tickets = new Queue<string>();
+
+            tickets.Enqueue("Poruchkata mi e greshna");
+            tickets.Enqueue("Poruchkata mi q izqde dostavchika");
+            tickets.Enqueue("Hranata mi grumna");
+
+            //Console.WriteLine(tickets.Dequeue());
+            //Console.WriteLine(tickets.Dequeue());
+
+            while (tickets.Count > 0) 
             {
-                int topElement = newStack.Pop();
-                Console.WriteLine(topElement);
+                string element = tickets.Dequeue();
+
+                Console.WriteLine(element);
             }
         }
     }
