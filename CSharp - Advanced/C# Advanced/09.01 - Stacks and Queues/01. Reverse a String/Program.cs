2899768d57a0620b0ef9c2127fe877f9246bed73
TsAngelov <ts.angelov06@gmail.com>
2024-01-10 20:30:34 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/09.01 - Stacks and Queues/01. Reverse a String/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> text = new Stack<char>(Console.ReadLine().ToCharArray());
+
+            //string input = Console.ReadLine();
+            //foreach (var item in input)
+            //{
+            //    text.Push(item);
+            //}
+
+            while (text.Count > 0)
+            {
+                Console.Write(text.Pop());
+            }
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/09.01 - Stacks and Queues/demo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> newStack = new Stack<int>();
+
+            newStack.Push(5);
+            newStack.Push(53);
+            newStack.Push(1);
+            newStack.Push(17);
+
+            //int topElement = newStack.Pop();
+
+            //Console.WriteLine(topElement);
+
+            //Console.WriteLine(newStack.Pop());
+
+            while (newStack.Count != 0)
+            {
+                int topElement = newStack.Pop();
+                Console.WriteLine(topElement);
+            }
+        }
+    }
+}
