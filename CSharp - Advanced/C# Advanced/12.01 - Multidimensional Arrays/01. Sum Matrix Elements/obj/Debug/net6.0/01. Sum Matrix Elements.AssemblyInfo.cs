892faa3f4ce0bff4ba57b560f7d0a92748589e41
TsAngelov <ts.angelov06@gmail.com>
2024-01-12 21:24:02 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/01. Sum Matrix Elements/obj/Debug/net6.0/01. Sum Matrix Elements.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/01. Sum Matrix Elements/obj/Debug/net6.0/01. Sum Matrix Elements.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Sum Matrix Elements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e035a15798e3e6a1397f07d4df3c0c658142095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f89a54cef24c0b1288f5935971023c536029fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Sum Matrix Elements")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Sum Matrix Elements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/MatrixPrinting/obj/Debug/net6.0/MatrixPrinting.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/MatrixPrinting/obj/Debug/net6.0/MatrixPrinting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatrixPrinting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e035a15798e3e6a1397f07d4df3c0c658142095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f89a54cef24c0b1288f5935971023c536029fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatrixPrinting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatrixPrinting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
