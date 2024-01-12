b75fc61f8cd910ef1fa77f38371ca51dcf4fa03c
TsAngelov <ts.angelov06@gmail.com>
2024-01-12 20:41:11 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/MatrixPrinting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MatrixPrinting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = // new int[4, 3]
+            {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 },
+                {10,11,12 },
+            };
+
+            Console.WriteLine($"Rows: {matrix.GetLength(0)}");
+            Console.WriteLine($"Cols: {matrix.GetLength(1)}");
+
+            for (int row = 0; matrix.GetLength(0) < 4; row++)
+            {
+
+                for (int col = 0; matrix.GetLength(1) < 3; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/12.01 - Multidimensional Arrays/ReadMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ReadMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            //for (int row = 0; row < rows; row++) 
+            //{
+            //    for (int col = 0; col < cols; col++)
+            //    {
+            //        matrix[row,col] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arrayRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arrayRow[col];
+                }
+            }
+        }
+    }
+}
