9fc66972bd6a5b9ff9351f6be06947f462e034d5
TsAngelov <ts.angelov06@gmail.com>
2024-01-12 02:10:31 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/11.01 - Exercise Stacks and Queues/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int s = int.Parse(input[1]);
+            int x = int.Parse(input[2]);
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int min = int.MaxValue;
+                    foreach (var item in stack)
+                    {
+                        if (item < min)
+                        {
+                            min = item;
+                        }
+                    }
+                    Console.WriteLine(min);
+                }
+            }
+        }
+    }
+}
