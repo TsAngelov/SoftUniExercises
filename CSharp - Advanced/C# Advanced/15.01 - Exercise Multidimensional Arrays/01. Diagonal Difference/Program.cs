747d0e1858940e69a03ffa235bad36dc866f22ee
TsAngelov <ts.angelov06@gmail.com>
2024-01-15 19:34:17 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/15.01 - Exercise Multidimensional Arrays/01. Diagonal Difference/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n,n];
+            for (int row = 0; row < n; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row,col] = currentRow[col];
+                }
+            }
+
+            int primaryDiagSum = 0;
+            int secondaryDiagSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagSum += matrix[i,i];
+                secondaryDiagSum += matrix[i, n - i - 1];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagSum - secondaryDiagSum));
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/15.01 - Exercise Multidimensional Arrays/02. Squares in Matrix/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
