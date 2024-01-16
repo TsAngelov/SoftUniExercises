cb19e5a4eb6e17c2bc9c3f2dbf0ea483def010c8
TsAngelov <ts.angelov06@gmail.com>
2024-01-16 20:42:03 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/02. Average Student Grades/obj/Debug/net6.0/02. Average Student Grades.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/02. Average Student Grades/obj/Debug/net6.0/02. Average Student Grades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbd65ec1036b2a6a13a6d48aba868c6eab7f6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8497c7f59c8be1578131f830dd9e0ad8cbce9316")]
 [assembly: System.Reflection.AssemblyProductAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/06. Record Unique Names/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine(String.Join("\n", names));
+        }
+    }
+}
