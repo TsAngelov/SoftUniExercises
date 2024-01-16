8497c7f59c8be1578131f830dd9e0ad8cbce9316
TsAngelov <ts.angelov06@gmail.com>
2024-01-16 20:27:18 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/02. Average Student Grades/obj/Debug/net6.0/02. Average Student Grades.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/02. Average Student Grades/obj/Debug/net6.0/02. Average Student Grades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3342a22f3a4cc52b4c2d683d062881ef46eb2205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbd65ec1036b2a6a13a6d48aba868c6eab7f6ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02. Average Student Grades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Advanced/C# Advanced/16.01 - Sets and Dictionaries/04. Product Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _04._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string,double>> stores = new Dictionary<string, Dictionary<string,double>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Revision")
+            {
+                string[] info = input.Split(' ');
+                
+                string store = info[0];
+                string product = info[1];
+                double price = double.Parse(info[2]);
+
+                if (!stores.ContainsKey(store))
+                {
+                    stores.Add(store, new Dictionary<string, double>());
+                }
+
+                if (!stores[store].ContainsKey(product))
+                {
+                    stores[store].Add(product, 0);
+                }
+
+                stores[store][product] = price;
+            }
+
+            stores = stores.OrderBy(s => s.Key).ToDictionary(s => s.Key, s => s.Value);
+
+            foreach (var (store, products) in stores)
+            {
+                Console.WriteLine($"{store} ->");
+
+                foreach (var (product,price) in products)
+                {
+                    Console.WriteLine($"Product: {product}, Price: {price}");
+                }
+            }
+        }
+    }
+}
