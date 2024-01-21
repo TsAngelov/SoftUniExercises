ff270a7c1235ae63a3b7e31c66ae2b57de53f867
TsAngelov <ts.angelov06@gmail.com>
2024-01-21 19:46:37 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/18.01 - Exercise Sets and Dictionaries/01. Unique Usernames/obj/Debug/net6.0/01. Unique Usernames.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/18.01 - Exercise Sets and Dictionaries/01. Unique Usernames/obj/Debug/net6.0/01. Unique Usernames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Unique Usernames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabe2fea745174faec69a17e7c38a5b48a655288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55bdb594edd59cd4dca33a011ca927105605fa80")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Unique Usernames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Unique Usernames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Advanced/C# Advanced/18.01 - Exercise Sets and Dictionaries/04. Even Times/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, bool> dic = new();
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (dic.ContainsKey(num))
+                {
+                    if (dic[num] == false)
+                    {
+                        dic[num] = true;
+                    }
+                    else
+                    {
+                        dic[num] = false;
+                    }
+                    continue;
+                }
+                dic.Add(num, false);
+            }
+            foreach (var num in dic) 
+            {
+                if (num.Value == true)
+                {
+                    Console.WriteLine(num.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
