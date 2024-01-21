55bdb594edd59cd4dca33a011ca927105605fa80
TsAngelov <ts.angelov06@gmail.com>
2024-01-21 19:33:20 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/18.01 - Exercise Sets and Dictionaries/03. Periodic Table/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> periodicTable = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (string s in input) 
+                {
+                    periodicTable.Add(s);
+                }
+            }
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
