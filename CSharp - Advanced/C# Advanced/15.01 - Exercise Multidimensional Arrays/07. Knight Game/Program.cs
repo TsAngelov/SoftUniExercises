971e9fabf7f553592f1009869b5c931d0cada0c7
TsAngelov <ts.angelov06@gmail.com>
2024-01-23 19:39:34 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/15.01 - Exercise Multidimensional Arrays/07. Knight Game/Program.cs
+++ CSharp - Advanced/C# Advanced/15.01 - Exercise Multidimensional Arrays/07. Knight Game/Program.cs
@@ -7,16 +7,74 @@
             int n = int.Parse(Console.ReadLine());
             char[,] matrix = new char[n, n];
 
-            for (int row = 0; row < n; row++)
+            for (int i = 0; i < n; i++)
             {
-                char[] matrixInput = Console.ReadLine().Split().Select(char.Parse).ToArray();
-                for (int col = 0; col < n; col++)
+                string input = Console.ReadLine();
+                for (int j = 0; j < n; j++)
                 {
-                    matrix[row, col] = matrixInput[col];
+                    matrix[i, j] = input[j];
                 }
             }
 
+            int knightsRemoved = 0;
+            while (true)
+            {
+                int countMostAttacking = 0;
+                int rowMostAttacking = 0, colMostAttacking = 0;
 
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int attackKnights = CountAttackedKnights(row, col);
+                            if (countMostAttacking < attackKnights)
+                            {
+                                countMostAttacking = attackKnights;
+                                rowMostAttacking = row;
+                                colMostAttacking = col;
+                            }
+                        }
+
+                    }
+                }
+
+                if (countMostAttacking == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    matrix[rowMostAttacking, colMostAttacking] = '0';
+                    knightsRemoved++;
+                }
+            }
+            Console.WriteLine(knightsRemoved);
+
+        }
+        int CountAttackedKnights(int row, int col, char[] matrix,)
+        {
+            int attackedKnights = 0;
+
+            // up-left
+            if (IsCellValid(row - 1, col - 2))
+            {
+                if ()
+                {
+
+                }
+            }
+            return attackedKnights;
+        }
+
+        bool IsCellValid(int row, int col)
+        {
+            return
+                row >= 0
+                && row < n
+                && col >= 0
+                && col < n;
         }
     }
 }
---
+++ CSharp - Advanced/C# Advanced/23.01 - Functional Programming/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .Where(n => n%2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+                
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
