95268438eb0574b6f45e398a1a780ec5b3495b68
TsAngelov <ts.angelov06@gmail.com>
2024-01-24 11:36:21 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/23.01 - Functional Programming/02. Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/23.01 - Functional Programming/DelegateExample/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DelegateExample
+{
+    internal class Program
+    {
+        public delegate int DoMath(int a, int b);
+        static void Main(string[] args)
+        {
+            //  Single cast delegate
+            // DoMath doMath = Add;
+            // Console.WriteLine(doMath(5, 6));
+
+            //  Multicast delegate
+            DoMath doMath = Add;
+            doMath += Multiply;
+            doMath += Calculate;
+
+            doMath(5, 7); // all 3 methods in 1
+        }
+        public static int Add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            Console.WriteLine(a*b);
+            return a * b;
+        }
+        public static int Calculate(int a, int b)
+        {
+            Console.WriteLine(a * a + b * b);
+            return a*a + b * b;
+        }
+    }
+}
