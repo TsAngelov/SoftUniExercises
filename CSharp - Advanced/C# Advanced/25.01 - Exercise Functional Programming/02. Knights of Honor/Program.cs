4b877fa5483d0bc0c2fd1f2448deb9dbaff96b7e
TsAngelov <ts.angelov06@gmail.com>
2024-01-25 21:28:29 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/02. Knights of Honor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printSir = name => Console.WriteLine($"Sir {name}");
+
+            Array.ForEach( input, printSir );
+        }
+    }
+}
