b0426ffb0dc18bbefeaa347a8eedb32c1b99ae9f
TsAngelov <ts.angelov06@gmail.com>
2024-01-26 01:49:07 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/01. Action Point/obj/Debug/net6.0/01. Action Point.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/01. Action Point/obj/Debug/net6.0/01. Action Point.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Action Point")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66e3cdb019a1faba68d453d2bc6b291a2bb4b2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+615291e7d69d867b6db639015637a12c3603083b")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Action Point")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Action Point")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/07. Predicate for Names/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _07._Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            Predicate<string> lengthCheck = name => name.Length == length;
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(n => lengthCheck(n)).ToArray();
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
