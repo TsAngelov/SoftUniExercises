55d91aac6ff83fc866be8d9c789c4593bd4cf95d
TsAngelov <ts.angelov06@gmail.com>
2024-01-25 21:21:35 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/01. Action Point/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = name => Console.WriteLine(name);
+
+            Array.ForEach(input, print);
+        }
+    }
+}
