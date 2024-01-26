2adae98e5044b4ec878d5f975ea80a217141cae0
TsAngelov <ts.angelov06@gmail.com>
2024-01-26 02:02:46 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/07. Predicate for Names/Program.cs
+++ CSharp - Advanced/C# Advanced/25.01 - Exercise Functional Programming/07. Predicate for Names/Program.cs
@@ -5,9 +5,12 @@
         static void Main(string[] args)
         {
             int length = int.Parse(Console.ReadLine());
-            Predicate<string> lengthCheck = name => name.Length == length;
+            Predicate<string> lengthCheck = name => name.Length <= length;
             string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(n => lengthCheck(n)).ToArray();
-            Console.WriteLine(string.Join("\n", names));
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
