9d337e15005c2abb626b64c5d70d268743189ddc
TsAngelov <ts.angelov06@gmail.com>
2024-01-29 21:51:47 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/26.01 - Defining Classes/01. Car/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer;
+
+public class Car
+{
+	private string make;
+	private string model;
+    private int year;
+    public string Make
+	{
+		get { return this.make; }
+		set { this.make = value; }
+	}
+    public string Model
+    {
+        get { return this.model; }
+        set { this.model = value; }
+    }
+    public int Year
+    {
+        get { return this.year; }
+        set { this.year = value; }
+    }
+}
+
+
---
+++ CSharp - Advanced/C# Advanced/26.01 - Defining Classes/demo/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace demo
+{
+    public class Person
+    {
+        private string name = "Ivan";
+        private int age = 5;
+
+        public string Name
+        {
+            get
+            { 
+                return name; 
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid name");
+                }
+                name = value;
+            }
+        }
+
+    }
+}
