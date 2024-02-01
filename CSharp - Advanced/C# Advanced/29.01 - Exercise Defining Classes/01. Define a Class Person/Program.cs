e2cd11dd817fa194ac8642357e5eb08d7663e8f9
TsAngelov <ts.angelov06@gmail.com>
2024-02-01 12:51:13 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/29.01 - Exercise Defining Classes/01. Define a Class Person/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "Peter";
+        person.Age = 20;
+
+        Person person2 = new Person();
+        person2.Name = "George";
+        person2.Age = 18;
+
+        Person person3 = new Person();
+        person3.Name = "Jose";
+        person3.Age = 43;
+    }
+}
