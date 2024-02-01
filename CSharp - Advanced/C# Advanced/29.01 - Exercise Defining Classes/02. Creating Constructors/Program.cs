a6c2bd6bdc57ab334aaa17875245346b9675e6ee
TsAngelov <ts.angelov06@gmail.com>
2024-02-01 12:55:43 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/29.01 - Exercise Defining Classes/02. Creating Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        Person person1 = new Person(2);
+
+        Person person2 = new Person("petar", 3);
+    }
+}
