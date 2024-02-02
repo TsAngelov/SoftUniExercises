2df88ee9f7e0396beb570564efe7cf01da7d3b1b
TsAngelov <ts.angelov06@gmail.com>
2024-02-02 20:15:43 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/02.02 - Generics/02. Generic Array Creator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericArrayCreator;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(string.Join(", ", ArrayCreator.Create(50, "Fifty")));
+    }
+}
