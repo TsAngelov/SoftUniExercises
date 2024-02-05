c1d9f910d5a9c91bafc3d763d243051d99e8ea6a
TsAngelov <ts.angelov06@gmail.com>
2024-02-05 21:37:24 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/05.02 - Exercise Generics/07. Tuple/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tuple;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nameInput = Console.ReadLine().Split();
+        CustomTuple<string, string> names = new($"{nameInput[0]} {nameInput[1]}", nameInput[2]);
+
+        string[] beerInput = Console.ReadLine().Split();
+        CustomTuple<string, double> beer = new(beerInput[0], double.Parse(beerInput[1]));
+
+        string[] typeInput = Console.ReadLine().Split();
+        CustomTuple<int, double> intDouble = new(int.Parse(typeInput[0]), double.Parse(typeInput[1]));
+
+        Console.WriteLine(names);
+        Console.WriteLine(beer);
+        Console.WriteLine(intDouble);
+    }
+}
