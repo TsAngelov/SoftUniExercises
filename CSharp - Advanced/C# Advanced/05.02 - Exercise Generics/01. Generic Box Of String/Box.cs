bd97ca3218b5419f31f4975724a654ae217b3657
TsAngelov <ts.angelov06@gmail.com>
2024-02-05 20:48:16 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/05.02 - Exercise Generics/01. Generic Box Of String/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace StringBox;
+
+public class Box<T>
+{
+    private List<T> list;
+
+    public Box()
+    {
+        list = new List<T>();
+    }
+    public void Add(T item) 
+    {
+        list.Add(item);
+    }
+    public override string ToString() 
+    {
+        var sb = new StringBuilder();
+        foreach (var item in list)
+        {
+            sb.AppendLine($"{item.GetType()}: {item}");
+        }
+        return sb.ToString().TrimEnd();
+    }
+}
---
+++ CSharp - Advanced/C# Advanced/05.02 - Exercise Generics/01. Generic Box Of String/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringBox;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<string> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            box.Add(Console.ReadLine());
+        }
+
+        Console.WriteLine(box);
+    }
+}
