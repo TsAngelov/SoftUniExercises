fd969e153c59d25ca1c02c56f8587e292ebeebfe
TsAngelov <ts.angelov06@gmail.com>
2024-02-05 20:50:47 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/05.02 - Exercise Generics/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IntegerBox;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<int> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            box.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine(box);
+    }
+}
