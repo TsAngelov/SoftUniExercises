d8053903c94e07351a3684cce5b49e1a7bf3a12d
TsAngelov <ts.angelov06@gmail.com>
2024-02-08 23:38:36 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/17. Iterators and Comparators/04. BookComparer/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+
+        public int CompareTo(Book? other)
+        {
+            int yearsComparison = Year.CompareTo(other.Year);
+            if (yearsComparison == 0) 
+            {
+                return Title.CompareTo(other.Title);
+            }
+            return yearsComparison; 
+        }
+        public override string ToString() 
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
