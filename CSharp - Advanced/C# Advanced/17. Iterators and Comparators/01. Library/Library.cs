8f5fa496197fedfea075985e55c11f0847a50b77
TsAngelov <ts.angelov06@gmail.com>
2024-02-08 22:01:35 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/17. Iterators and Comparators/01. Library/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> Books;
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
