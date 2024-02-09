95834830bb73e3409afe816b67c72fffb6facf7f
TsAngelov <ts.angelov06@gmail.com>
2024-02-09 02:22:56 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/18. Exercise Iterators and Comparators/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int index = 0;
+
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public bool HasNext() 
+        {
+            return index < list.Count - 1;
+        }
+
+        public bool Move() 
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+        public void Print() 
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+                Console.WriteLine(list[index]);
+        }
+    }
+}
