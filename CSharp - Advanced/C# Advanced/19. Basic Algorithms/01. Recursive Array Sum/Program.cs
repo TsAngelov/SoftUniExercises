f7ac590419b926eac11d318794e6910ce19a483e
TsAngelov <ts.angelov06@gmail.com>
2024-02-13 20:49:29 +0200

    New Solution

---
+++ CSharp - Advanced/C# Advanced/19. Basic Algorithms/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(ArraySum(array));
+        }
+
+        static int ArraySum(int[] array)
+        {
+            if (array.Length == 1)
+            {
+                return array[0];
+            }
+
+            int[] restArray = array.Skip(1).ToArray();
+
+            return array[0] + ArraySum(restArray);
+        }
+    }
+}
