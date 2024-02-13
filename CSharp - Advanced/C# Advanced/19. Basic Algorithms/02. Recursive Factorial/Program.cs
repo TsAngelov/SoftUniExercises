f4edb3d6b1b38e7b29e1ea2e07ba9dd2ed25a929
TsAngelov <ts.angelov06@gmail.com>
2024-02-13 20:49:51 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/19. Basic Algorithms/02. Recursive Factorial/Program.cs
+++ CSharp - Advanced/C# Advanced/19. Basic Algorithms/02. Recursive Factorial/Program.cs
@@ -5,6 +5,19 @@ namespace _02._Recursive_Factorial
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
         }
     }
 }
