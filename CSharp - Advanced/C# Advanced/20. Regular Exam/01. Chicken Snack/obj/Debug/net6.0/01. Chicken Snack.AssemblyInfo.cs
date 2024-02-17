34614d7887f84a6913dc552338c106416aa2618f
TsAngelov <ts.angelov06@gmail.com>
2024-02-17 10:36:57 +0200

    New Solution

--- CSharp - Advanced/C# Advanced/20. Regular Exam/01. Chicken Snack/obj/Debug/net6.0/01. Chicken Snack.AssemblyInfo.cs
+++ CSharp - Advanced/C# Advanced/20. Regular Exam/01. Chicken Snack/obj/Debug/net6.0/01. Chicken Snack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Chicken Snack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16798660236c02f41e0852a3f999a2ba9d7f2660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c16ffafafeb3696f566d726e606173f2dde6b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Chicken Snack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Chicken Snack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CSharp - Advanced/C# Advanced/20. Regular Exam/03. SharkTaxonomy/Classifier.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public int Capacity { get; set; }
+        public List<Shark> Species {  get; set; }
+        public int GetCount { get => Species.Count; }
+        public Classifier(int capacity)
+        {
+            Capacity = capacity;
+            Species = new List<Shark>();
+        }
+        public void AddShark(Shark shark)
+        {
+            if (!Species.Any(x => x.Kind == shark.Kind))
+            {
+                if (Species.Count < Capacity)
+                {
+                    Species.Add(shark);
+                }
+            }
+        }
+        public bool RemoveShark(string kind) => Species.Remove(Species.FirstOrDefault(x => x.Kind == kind));
+        public string GetLargestShark() => Species.OrderByDescending(x => x.Length).FirstOrDefault().ToString();
+        public double GetAverageLength() => Species.Average(x => x.Length);
+        public string Report()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"{GetCount} sharks classified:");
+            foreach (var shark in Species) 
+            {
+                sb.AppendLine(shark.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
