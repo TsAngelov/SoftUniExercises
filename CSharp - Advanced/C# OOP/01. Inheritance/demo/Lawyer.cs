a20716e9998a60e333c01214a06cee68116d0dde
TsAngelov <ts.angelov06@gmail.com>
2024-02-20 19:42:37 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/01. Inheritance/demo/Lawyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo;
+
+public class Lawyer : Person
+{
+    public string Company { get; set; }
+}
---
+++ CSharp - Advanced/C# OOP/01. Inheritance/demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new();
+            student.Address = "Drujbai 1";
+            student.LastName = "Patronov";
+            student.University = "Vazov";
+
+            Console.WriteLine(student.Address);
+            Console.WriteLine(student.LastName);
+            Console.WriteLine(student.University);
+        }
+    }
+}
