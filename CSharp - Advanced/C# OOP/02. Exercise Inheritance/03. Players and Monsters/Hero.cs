b664994e904e741efa8ec5f61127403ca5e8625c
TsAngelov <ts.angelov06@gmail.com>
2024-02-22 20:00:22 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/02. Exercise Inheritance/03. Players and Monsters/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+        public Hero(string username, int level)
+        {
+            Username = username;
+            Level = level;
+        }
+        public override string ToString() 
+        {
+            return $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+        }
+    }
+}
