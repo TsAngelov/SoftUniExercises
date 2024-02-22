5f461676de23715fea49efde1b68cecd907d0b72
TsAngelov <ts.angelov06@gmail.com>
2024-02-22 19:37:10 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/02. Exercise Inheritance/01. Person/Child.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        { }
+
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("A child cannot be older than 15!");
+                }
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/02. Exercise Inheritance/01. Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        private int age;
+        public string Name { get; set; }
+        public virtual int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative!");
+                }
+                age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
