337427161b1e0ff81a6e71930f892be1eb10e75b
TsAngelov <ts.angelov06@gmail.com>
2024-02-26 19:21:05 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/04. Exercise Encapsulation/01. Class Box Data/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length 
+        { 
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            } 
+        }
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public double SurfaceArea()
+        {
+            return 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+    }
+}
