a31060e82302ec808efa68155af1f955e5ab7338
TsAngelov <ts.angelov06@gmail.com>
2024-03-01 17:33:45 +0200

    New Solution

--- CSharp - Advanced/C# OOP/06. Exercise Interfaces and Abstraction/01. Define an Interface IPerson/obj/Debug/net6.0/01. Define an Interface IPerson.AssemblyInfo.cs
+++ CSharp - Advanced/C# OOP/06. Exercise Interfaces and Abstraction/01. Define an Interface IPerson/obj/Debug/net6.0/01. Define an Interface IPerson.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Define an Interface IPerson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a335468d27fdcdca51a09c1c8fceff617a54b5ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf0b9de2f08ee6c5ee081e7d7ac4cfd78f369c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Define an Interface IPerson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Define an Interface IPerson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
