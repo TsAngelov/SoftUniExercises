133b1df0115b28534b26ecb51e71b8f214070c16
TsAngelov <ts.angelov06@gmail.com>
2024-03-12 21:39:10 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/09. Exception Handling/01. Square Root/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
