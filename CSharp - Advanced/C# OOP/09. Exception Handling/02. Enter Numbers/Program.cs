465e870500fe7ceaac8fd365aa223d6aa9afeea3
TsAngelov <ts.angelov06@gmail.com>
2024-03-13 02:04:16 +0200

    New Solution

---
+++ CSharp - Advanced/C# OOP/09. Exception Handling/02. Enter Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+
+            int[] numbers = new int[10];
+            int currentIndex = 0;
+            while (numbers[numbers.Length - 1] == default) // Until the last index is filled
+            {
+                try
+                {
+                    int currentNumber = ReadNumber(start, end);
+                    start = currentNumber;
+                    numbers[currentIndex] = currentNumber;
+                    currentIndex++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+            }
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+        public static int ReadNumber(int start, int end)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start || number >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+            return number;
+        }
+    }
+}
---
+++ CSharp - Advanced/C# OOP/09. Exception Handling/03. Cards/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace _03._Cards
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] cards = Console.ReadLine().Split(", ");
+            List<Card> validCards = new List<Card>();
+
+            foreach (var card in cards)
+            {
+                string[] splitCard = card.Split();
+                try
+                {
+                    string face = splitCard[0];
+                    char suit = char.Parse(splitCard[1]);
+                    validCards.Add(CreateCard(face, suit));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid card!");
+                }
+
+            }
+            validCards.ForEach(card => Console.Write(card + " "));
+        }
+        public static Card CreateCard(string face, char suit)
+        {
+            IReadOnlyCollection<string> ValidFaces = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            IReadOnlyCollection<char> ValidSuits = new List<char>() { 'S', 'H', 'D', 'C' };
+
+            if (!ValidFaces.Contains(face))
+            {
+                throw new ArgumentException();
+            }
+            else if (!ValidSuits.Contains(suit))
+            {
+                throw new ArgumentException();
+            }
+            return new Card(face, suit);
+        }
+    }
+    public class Card
+    {
+        private char suit;
+        public Card(string face, char suit)
+        {
+            Face = face;
+            Suit = suit;
+        }
+
+        public string Face { get; set; }
+        public char Suit
+        {
+            get => suit;
+            set
+            {
+                suit = value switch
+                {
+                    'S' => '\u2660',
+                    'H' => '\u2665',
+                    'D' => '\u2666',
+                    'C' => '\u2663',
+                };
+            }
+        }
+        public override string ToString()
+        {
+            return $"[{Face}{Suit}]";
+        }
+    }
+}
