48709f9329cbfd27593e0da944fa4431f472a030
TsAngelov <ts.angelov06@gmail.com>
2024-04-02 00:06:53 +0300

    New Solution

---
+++ CSharp - Advanced/C# OOP/13. Reflection and Attributes/05. Create Attribute/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace AuthorProblem
+{
+    public class StartUp
+    {
+        [Author("Victor")]
+        public static void Main(string[] args)
+        {
+        }
+
+        [Author("Patrona")]
+        [Author("Petar")]
+        [Author("Tiras")]
+        public void NewMethod()
+        {
+
+        }
+    }
+}
