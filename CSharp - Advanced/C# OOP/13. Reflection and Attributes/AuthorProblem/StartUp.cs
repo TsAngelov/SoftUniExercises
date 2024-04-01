f23643931f0dcc4ecc6a52470fbc75ea323bcc91
TsAngelov <ts.angelov06@gmail.com>
2024-04-01 23:58:48 +0300

    New Solution

---
+++ CSharp - Advanced/C# OOP/13. Reflection and Attributes/AuthorProblem/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace AuthorProblem
+{
+    public class StartUp
+    {
+        [Author("Victor")]
+        public static void Main(string[] args)
+        {
+            new Tracker().PrintMethodsByAuthor();
+        }
+
+        [Author("Patrona")]
+        [Author("Petar")]
+        [Author("Tiras")]
+        public void NewMethod()
+        {
+
+        }
+    }
+}
