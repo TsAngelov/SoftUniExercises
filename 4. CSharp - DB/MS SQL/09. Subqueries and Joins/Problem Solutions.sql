8790420805cadf1135b7adf7f15a3e4f6b3970e2
TsAngelov <ts.angelov06@gmail.com>
2024-06-14 02:31:16 +0300

    New Solution

--- 4. CSharp - DB/MS SQL/09. Subqueries and Joins/Problem Solutions.sql
+++ 4. CSharp - DB/MS SQL/09. Subqueries and Joins/Problem Solutions.sql
@@ -1,6 +1,7 @@
 USE SoftUni
 GO
 
+-- 1.
 SELECT TOP(50)
 	   e.FirstName,
 	   e.LastName,
@@ -11,3 +12,61 @@ SELECT TOP(50)
   JOIN Towns as t ON t.TownID = a.TownID
  ORDER BY FirstName,
 		  LastName
+
+-- 2.
+SELECT e.EmployeeID,
+	   e.FirstName,
+	   e.LastName,
+	   d.[Name] AS DepartmentName
+  FROM Employees AS e
+  JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+ WHERE d.[Name] = 'Sales'
+ ORDER BY e.EmployeeID
+
+-- 3.
+SELECT e.FirstName,
+	   e.LastName,
+	   e.HireDate,
+	   d.[Name] AS DeptName
+  FROM Employees AS e
+  JOIN Departments AS d ON d.DepartmentID = e.DepartmentID 
+ WHERE 
+		HireDate > '1/1/1999'
+		AND d.[Name] IN ('Sales', 'Finance')
+  ORDER BY e.HireDate
+
+-- 4.
+SELECT TOP(50)
+	   e.EmployeeID,
+	   CONCAT_WS(' ', e.FirstName, e.LastName) AS EmployeeName, 
+	   CONCAT_WS(' ', em.FirstName, em.LastName) AS ManagerName,
+	   d.[Name] AS DepartmentName
+  FROM Employees AS e
+  JOIN Employees AS em ON e.ManagerID = em.EmployeeID
+  JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+ ORDER BY e.EmployeeID
+
+-- 5.
+SELECT TOP(1) 
+       AVG(Salary) AS AvgSalary
+  FROM Employees
+ GROUP BY DepartmentId
+ ORDER BY AvgSalary
+
+ -- Subquery
+SELECT MIN(dt.AvgSalary) AS MinAvgSalary
+  FROM (SELECT AVG(Salary) AS AvgSalary
+		  FROM Employees
+		 GROUP BY DepartmentId) AS dt
+
+ -- Common Table Expression
+WITH CTE_MinAvgSalary
+     (AvgSalary)
+  AS
+  (
+	SELECT AVG(Salary) AS AvgSalary
+	FROM Employees
+	GROUP BY DepartmentId
+   )
+
+SELECT MIN(AvgSalary) FROM CTE_MinAvgSalary
